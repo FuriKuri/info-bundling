22ba4ec0b84fae307900ff4ccc191712f1671325
Theo Pack <tf.pack@gmail.com>
2015-02-06 09:11:48 +0100

    Configure port

--- cli.js
+++ cli.js
@@ -1,5 +1,19 @@
 #!/usr/bin/env node
-
+var meow = require('meow');
 var Server = require('./lib');
-console.log('Starting standalone server on port 1337...');
-new Server().startServer(1337);
+
+var cli = meow({
+  help: [
+    'Usage',
+    '  info-bundling <options>',
+    '',
+    'Options',
+    '  --port <port>    Port which the server is listen to. Default port is 1337.'
+  ].join('\n')
+});
+var port = 1337;
+if (cli.flags.port) {
+  port = cli.flags.port;
+}
+console.log('Starting standalone server on port ' + port + ' ...');
+new Server().startServer(port);
